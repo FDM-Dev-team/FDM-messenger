57dac8b17b5f4cc60a4087c98cb34d8189b38835
Mak <yilut.mak@fdmgroup.com>
2024-03-12 23:39:24 +0800

    added socket io

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,25 +1,67 @@
-import React from "react";
 import "./Chat.css";
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 
-export default function Chat() {
+const socket = io('http://localhost:9000/socket'); // Replace with your backend URL and endpoint
+
+const Chat = () => {
+  const [message, setMessage] = useState('');
+  const [chatLog, setChatLog] = useState([]);
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log('Connected to server');
+    });
+
+    socket.on('message', (data) => {
+      setChatLog((prevChatLog) => [...prevChatLog, data]);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  const sendMessage = () => {
+    socket.emit('message', message);
+    setMessage('');
+  };
+
   return (
     <div
-      className="col flex-grow-1 d-flex flex-column p-0"
-      style={{ height: "100vh" }}
+    className="col flex-grow-1 d-flex flex-column p-0"
+    style={{ height: "100vh" }}
+  >
+    <div
+      className=" p-0 rounded"
+      style={{
+        marginBottom: "15px",
+        marginTop: "15px",
+        marginRight: "15px",
+        marginLeft: "5px",
+        flex: "1",
+        boxShadow: "-4px 4px 10px rgba(0, 0, 0, 0.4)",
+      }}
     >
-      <div
-        className=" p-0 rounded"
-        style={{
-          marginBottom: "15px",
-          marginTop: "15px",
-          marginRight: "15px",
-          marginLeft: "5px",
-          flex: "1",
-          boxShadow: "-4px 4px 10px rgba(0, 0, 0, 0.4)",
-        }}
-      >
-        Hello
+      <div>
+      <div>
+        {chatLog.map((msg, index) => (
+          <div key={index}>{msg}</div>
+        ))}
       </div>
+      <input
+        type="text"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button onClick={sendMessage}>Send</button>
     </div>
+    </div>
+  </div>
+
+
+    
   );
-}
+};
+
+export default Chat;
