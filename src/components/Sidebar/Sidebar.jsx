ee155fd2e539e5057995a7b5bd1851f17eb55e28
Eric Chung <eric.chung@fdmgroup.com>
2024-03-14 16:12:43 +0800

    SideBar chat display and click to fetch chat msg

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,21 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Sidebar.css";
+import axios from "axios";
+import SideBarChats from "./SidebarChats/SidebarChats";
 
 export default function Sidebar() {
+  const [chatList, setChatList] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:9000/chat/list/1");
+        setChatList(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="row flex-grow-1 justify-content-center">
       <div
-        className="p-0 rounded"
+        className="p-0 rounded d-flex flex-column justify-content-start align-items-center"
         style={{
           width: "330px",
+          height: "calc(100vh - 150px)",
           marginBottom: "15px",
           marginTop: "10px",
           marginRight: "15px",
           marginLeft: "15px",
           boxShadow: "-4px 4px 10px rgba(0, 0, 0, 0.4)",
+          overflowY: "scroll",
         }}
       >
-        chats
+        <div
+          className="d-flex flex-column justify-content-start align-items-center"
+          style={{
+            height: "fit-content",
+            paddingTop: "10px",
+            paddingBottom: "10px",
+          }}
+        >
+          {chatList.map((chat) => (
+            <SideBarChats key={chat.id} chat={chat} />
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/Sidebar/SidebarChats/SidebarChats.jsx
+++ src/components/Sidebar/SidebarChats/SidebarChats.jsx
@@ -1,6 +1,50 @@
 import React from "react";
-import "./SideBarChats.css";
+import "./SidebarChats.css";
+import axios from "axios";
 
-export default function SideBarChats() {
-  return <div>SideBarChats</div>;
+export default function SideBarChats({ chat }) {
+  const fetchThatChatMessage = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:9000/chatmessage/${chat.chat_id}`
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div
+      className="d-flex align-items-center border my-1"
+      style={{
+        width: "300px",
+        height: "75px",
+        borderRadius: "50px",
+        padding: "10px",
+        cursor: "pointer",
+      }}
+      onClick={fetchThatChatMessage}
+    >
+      <div
+        className="circle"
+        style={{
+          width: "55px",
+          height: "55px",
+          borderRadius: "50%",
+          backgroundColor: "gray",
+          marginRight: "10px",
+        }}
+      ></div>
+      <div
+        className="d-flex align-items-center justify-content-center"
+        style={{
+          width: "220px",
+          height: "50px",
+        }}
+      >
+        {chat.name}
+      </div>
+    </div>
+  );
 }
