c58d13ec8797a338e114a510f7241f93503897e6
Eric Chung <eric.chung@fdmgroup.com>
2024-03-19 11:59:59 +0800

    Chat layout (rough)

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -8,7 +8,7 @@ export default function Chat() {
       style={{ height: "100vh" }}
     >
       <div
-        className=" p-0 rounded"
+        className="p-0 rounded"
         style={{
           marginBottom: "15px",
           marginTop: "15px",
@@ -16,9 +16,70 @@ export default function Chat() {
           marginLeft: "5px",
           flex: "1",
           boxShadow: "-4px 4px 10px rgba(0, 0, 0, 0.4)",
+          display: "flex",
+          flexDirection: "column",
         }}
       >
-        Hello
+        <div className="chatroom-header">
+          <div
+            className="circle"
+            style={{
+              width: "38px",
+              height: "38px",
+              borderRadius: "50%",
+              backgroundColor: "gray",
+              marginBlock: "6px",
+            }}
+          ></div>
+          <div className="chatroom-name">123</div>
+        </div>
+        <div className="flex-grow-1"></div>
+        <div
+          className="input-area"
+          style={{
+            height: "100px",
+            padding: "10px",
+            borderTop: "1px solid lightgray",
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <div
+            className="icon"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              width: "10%",
+            }}
+          >
+            <span style={{ cursor: "pointer" }}>+</span>
+          </div>
+          <input
+            type="text"
+            placeholder="Enter your message"
+            style={{
+              width: "80%",
+              height: "50px",
+              padding: "15px",
+              borderRadius: "5px",
+              border: "1px solid lightgray",
+            }}
+          />
+          <div
+            className="icon"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              width: "10%",
+            }}
+          >
+            <span style={{ cursor: "pointer" }}>Send</span>
+          </div>
+        </div>
       </div>
     </div>
   );
