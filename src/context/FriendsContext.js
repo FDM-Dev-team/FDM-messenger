d94c37f29d60a2c4c6d757096ad217da7b5899b5
Azamat.Nurpeissov <azamat.nurpeissov@gmail.com>
2024-03-20 16:32:54 +0800

    profile

--- src/context/FriendsContext.js
+++ src/context/FriendsContext.js
@@ -1,20 +1,24 @@
 import React, { useState, createContext, useContext, useEffect } from 'react';
 import axios from 'axios';
+import { useUser } from './UserContext'; // Assuming you have a UserContext
 
 const friendsContext = createContext();
 
 export function FriendsProvider({ children }) {
   const [friends, setFriends] = useState([]);
+  const { user } = useUser(); // Get the user object from UserContext
 
   useEffect(() => {
-    fetchFriends();
-  }, []);
+    if (user) {
+      fetchFriends(user.user_id); // Pass the user's ID to the fetchFriends function
+    }
+  }, [user]);
 
-  const fetchFriends = async () => {
+  const fetchFriends = async (userId) => {
     try {
-      const response = await axios.get('http://localhost:9000/friends/1'); // Replace 'your-api-endpoint' with the actual API endpoint to get friends
+      const response = await axios.get(`http://localhost:9000/friends/${userId}`);
       setFriends(response.data);
-      console.log(response.data)
+      console.log(response.data);
     } catch (error) {
       console.error(error);
     }
