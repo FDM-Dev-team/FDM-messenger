1bcbc8bcf10d1bc895dc4aefac38f39af3d73ff5
kiki lam <kiki.lam@fdmgroup.com>
2024-03-20 15:28:35 +0800

    UserContext

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,32 +1,32 @@
-import { useState, createContext, useContext, useEffect } from 'react';
+import { useState, createContext, useContext, useEffect } from "react";
 
-const UserContext = createContext()
+const UserContext = createContext();
 
 function UserProvider({ children }) {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    const storedUser = JSON.parse(localStorage.getItem('user'))
-    setUser(storedUser)
-  }, [])
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    setUser(storedUser);
+  }, []);
 
   const getUser = () => {
-    return JSON.parse(localStorage.getItem('user'))
-  }
+    return JSON.parse(localStorage.getItem("user"));
+  };
 
   const userIsAuthenticated = () => {
-    return localStorage.getItem('user') !== null
-  }
+    return localStorage.getItem("user") !== null;
+  };
 
-  const userLogin = user => {
-    localStorage.setItem('user', JSON.stringify(user))
-    setUser(user)
-  }
+  const userLogin = (user) => {
+    localStorage.setItem("user", JSON.stringify(user));
+    setUser(user);
+  };
 
   const userLogout = () => {
-    localStorage.removeItem('user')
-    setUser(null)
-  }
+    localStorage.removeItem("user");
+    setUser(null);
+  };
 
   const contextData = {
     user,
@@ -34,19 +34,17 @@ function UserProvider({ children }) {
     userIsAuthenticated,
     userLogin,
     userLogout,
-  }
+  };
 
   return (
-    <UserContext.Provider value={contextData}>
-      {children}
-    </UserContext.Provider>
-  )
+    <UserContext.Provider value={contextData}>{children}</UserContext.Provider>
+  );
 }
 
-export default UserContext
+export default UserContext;
 
 export function useUser() {
-  return useContext(UserContext)
+  return useContext(UserContext);
 }
 
-export { UserProvider }
+export { UserProvider };
