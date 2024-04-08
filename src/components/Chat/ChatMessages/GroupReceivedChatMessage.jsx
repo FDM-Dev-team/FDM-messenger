820d9f3b5ef887127039c5a5f5624c2824469efd
Eric Chung <eric.chung@fdmgroup.com>
2024-04-08 16:11:15 +0800

    sender icon

--- src/components/Chat/ChatMessages/GroupReceivedChatMessage.jsx
+++ src/components/Chat/ChatMessages/GroupReceivedChatMessage.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import "./ChatMessages.css";
 
-function GroupReceivedChatMessage({ message, convertTime}) {
+function GroupReceivedChatMessage({ message, convertTime }) {
+  console.log(message)
   return (
     <div className="message received-message">
-      <div className='message-sender'>{message.sender_name}</div>
+      <div className="chatroom-table-cell-avatar">
+        <div className="chatroom-avatar" id="UserProfileCircle">
+          {message.sender_name}
+        </div>
+      </div>
       <div className="message-text">{message.text}</div>
       <div className="message-time">{convertTime(message.time)}</div>
     </div>
